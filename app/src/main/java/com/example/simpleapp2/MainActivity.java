60f2c4f415595e1425532611d20671a05bb9e99e
sachavieuxroy <mail@sachavieuxroy.com>
2016-03-02 06:13:54 +0100

    Initial commit

---
+++ app/src/main/java/com/example/simpleapp2/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.simpleapp2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.jdom2.Document;
+
+//import org.jdom2.Document;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Document doc = new Document();
+    }
+}
